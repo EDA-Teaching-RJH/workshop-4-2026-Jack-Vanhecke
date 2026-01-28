0fafa786479c28a556e64eb21f382d49301a63b1
Jack-Vanhecke <jv11@kent.ac.uk>
2026-01-28 12:00:55 +0000

    edited code - unsure of issue at current time

--- gradebook_fix.py
+++ gradebook_fix.py
@@ -1,44 +1,59 @@
+def append_list_names(names, change_name):
+
+    names.append(change_name)
+
+    return names
+
+def append_list_scores(scores, change_score):
+
+    scores.append(change_score)
+
+    return scores
+
 def main():
-    # Ask how many students are in the class
-    student_count = input("How many students to enter? ")
+    # Ask how many students are in the class - force to be integer instead of default string
+    student_count = int(input("How many students to enter? "))
 
     # Lists to store data
     names = []
     scores = []
 
-    # Loop to get student details
+    # Loop to get student details - force i+1 to string for concatination
     for i in range(student_count):
-        print("Student " + i + 1)
+        print("Student " + str(i + 1))
         
-        # Clean up the name input
-        name_input = input("Name: ").strip.title()
-        names.append(name_input)
+        # Clean up the name input - missing syntax () after .strip
+        name_input = input("Name: ").strip().title()
+        append_list_names(names, name_input)
 
         # Keep asking for score until valid (0-100)
         while True:
-            score_input = input("Score: ")
+            score_input = int(input("Score: ")) # Force input to integer data type rather than defaut string
             
             # Check for valid range
-            if score_input < 0 or score_input > 100:
+            if (score_input <= 0) or (score_input >=100):
                 print("Invalid score. Must be 0-100.")
                 continue
             else:
                 break
         
-        scores.append(score_input)
+        append_list_scores(scores, score_input)
 
     # Print results
     print("--- Class Summary ---")
     
     # Loop through the lists and print pass/fail
     # Pass mark is 40
+    i = 0
     for i in range(len(names)):
-        if scores(i) < 40:
+        if scores[i] < 40:
             result = "Fail"
+            i += 1 # Adds 1 to variable i each loop
         else:
             result = "Pass"
-        
-        print(f"{i + 1}: {names[i]} - {scores[i]} ({result})")
+            i += 1 # Adds 1 to variable i each loop
+        print(str(len(names)))
+        print(str(i + 1), ": " + str(names[i]), " - ", str(scores[i]), " - " + str(result))
 
-# Call the main function
-main
+# Call the main function - fixed syntax missing ()
+main()
